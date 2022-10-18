fe0dc1967062b84e86fbdf9d7152f2ff6401d1c1
hrmussa <andersgiovanni@gmail.com>
2022-10-18 09:38:33 +0200

    initial

---
+++ src/icd11_client.py
@@ -0,0 +1,16 @@
+import os
+
+import requests
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+
+class ICD11_Client:
+    def __init__(self) -> None:
+        pass
+
+
+if __name__ == "__main__":
+    pass
