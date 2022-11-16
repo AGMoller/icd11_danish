303d1ebf448a236ba4cbcd516b516b409e8179d3
hrmussa <andersgiovanni@gmail.com>
2022-11-16 20:40:14 +0100

    update runner

--- runner.sh
+++ runner.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-#SBATCH --job-name=icd11_scraper    # Job name
-#SBATCH --output=outputs/icd11_scraper.%j.out      # Name of output file (%j expands to jobId)
+#SBATCH --job-name=icd11_translator    # Job name
+#SBATCH --output=outputs/icd11_translator.%j.out      # Name of output file (%j expands to jobId)
 #SBATCH --cpus-per-task=16       # Schedule one core
 #SBATCH --time=24:00:00          # Run time (hh:mm:ss) - run for one hour max
-#SBATCH --partition=brown    # Run on either the Red or Brown queue
+#SBATCH --gres=gpu             # Request GPU
+#SBATCH --partition=brown,red    # Run on either the Red or Brown queue
 #SBATCH --mail-type=BEGIN,FAIL,END    # Send an email when the job finishes or fails
 
-python src/icd11_client.py
+python src/translator.py
+# python src/icd11_client.py
